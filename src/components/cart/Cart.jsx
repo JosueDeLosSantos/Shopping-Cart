8797b784ba54d36eac2e849fafa2249f837bf933
JosueDeLosSantos <jdd1993@hotmail.com>
2023-10-30 19:13:00 -0400

    Modify Cart module

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -3,43 +3,39 @@ import PropTypes from "prop-types"
 
 const Cart = (props) => {
 	const { all, quantity, changeQuantity } = props
-	const [cart, setCart] = useState([])
-
-	
-
-	useEffect(() => {
-		function itemFinder(id) {
-            const keys = Object.keys(all)
-            let answer = null
-            keys.forEach((key) => {
-                all[key].forEach((el) => {
-                    if (`${el.id}` === id) {
-                        console.log(el)
-                        answer = el
-                    }
-                })
+    const cart = []
+    
+    function itemFinder(id) {
+        const keys = Object.keys(all)
+        let answer = null
+        keys.forEach((key) => {
+            all[key].forEach((el) => {
+                if (`${el.id}` === id) {
+                    // console.log(el)
+                    answer = el
+                }
             })
-            return answer
-        }
+        })
+        return answer
+    }
 
-		const regex = /^\d+$/
-		for (let key in quantity) {
-			if (regex.test(quantity[key].quantity)) {
-				//console.log(quantity[key].id)
-				const item = itemFinder(quantity[key].id)
-				const amount = quantity[key].quantity
-				const newCart = cart
-				newCart.push({
-					item: item,
-					amount: Number(amount),
-					price: item.price,
-					total: item.price * Number(amount),
-				})
-				setCart(newCart)
-			}
-		}
-		console.log(cart)
-	}, [quantity, all, cart])
+    const regex = /^\d+$/
+    for (let key in quantity) {
+        if (regex.test(quantity[key].quantity)) {
+            //console.log(quantity[key].id)
+            const item = itemFinder(quantity[key].id)
+            const amount = quantity[key].quantity
+
+            cart.push({
+                item: item,
+                amount: Number(amount),
+                price: item.price,
+                total: item.price * Number(amount),
+            })
+        }
+    }
+
+	console.log(cart)
 
 	return (
 		<main>
